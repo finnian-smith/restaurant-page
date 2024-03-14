d9c81d9b095bc0e4748f2cbdc51b5a71beb26b0e
finnian-smith <finniansmith01@gmail.com>
2024-03-14 09:49:44 +0000

    Apply styling to home tab

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import homeImage from "../assets/images/restaurant-home-image.jpeg";
 import "../styles/main.css";
+import "../styles/home.css";
 
 function home() {
   const contentDiv = document.getElementById("content");
@@ -15,17 +16,20 @@ function home() {
   // description
   const description = document.createElement("p");
   description.textContent =
-    "Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse";
+    "Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse.";
+  description.classList.add("home-description");
   contentDiv.appendChild(description);
 
   // book table button
   const bookTableButton = document.createElement("button");
   bookTableButton.textContent = "Book a Table";
+  bookTableButton.classList.add("book-table-button");
   contentDiv.appendChild(bookTableButton);
 
   // image
   const mainImage = document.createElement("img");
   mainImage.src = homeImage;
+  mainImage.classList.add("home-image");
   contentDiv.appendChild(mainImage);
 }
 
